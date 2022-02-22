fc48030267b56bc4391f21dffbc71c3094351ff2
FilipWirsen <Filip.wirsen99@gmail.com>
2022-02-22 13:46:00 +0000

    Collect sales data for last 5 markets in column list

--- run.py
+++ run.py
@@ -82,6 +82,15 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def get_5_last_entries_sales():
+    
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
 
 def main():
     """
@@ -96,4 +105,5 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+sales_columns = get_5_last_entries_sales()
